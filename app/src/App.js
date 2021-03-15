e8e57d40c2385241647750510d104a3c69575e53
baptistemrt <contact@baptistemrt.fr>
2021-03-15 02:41:12 +0100

    :sparkles: Add landing page

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import logo from './logo.svg';
-import './App.css';
+import './assets/scss/index.scss';
+
 
 function App() {
+  function handleOpenGithubLink () {
+    window.open("https://github.com/baptistemrt/tarot-game.app", "_blank");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="App-logo">
+          <img src={logo} alt={"landing logo"} />
+        </div>
+        <div className="App-title">
+          <h1>Du tarot, point.</h1>
+          <h3>L'application est en développement...</h3>
+        </div>
+      </section>
+      <div className="github-button">
+        <button onClick={handleOpenGithubLink} >GitHub <FontAwesomeIcon icon={faGithub} /></button>
+      </div>
     </div>
   );
 }
