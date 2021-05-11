0ffbe9775e7b7d7afb24d570b3c7faef52b65b7a
baptistemrt <contact@baptistemrt.fr>
2021-05-12 00:19:38 +0200

    :sparkles: Add new table of content in rules

--- app/src/Components/Rules.js
+++ app/src/Components/Rules.js
@@ -1,10 +1,49 @@
+import "../assets/scss/components/Rules.scss";
+
 const Rules = () => {
   return (
     <>
 
       <section className="page">
-        <section className="">
-          Rules
+        <section className="rules-page">
+          <section className="rules-table-of-content">
+            <div className="table">
+              <h3>Table des matières</h3>
+              <ul>
+                <li className="rule-title-list"><h4>Règles du tarot</h4></li>
+
+                <li className="rule-sublist">Introduction</li>
+                <li className="rule-sublist">Distribution</li>
+                <li className="rule-sublist">Déroulement d'un donne</li>
+                <li className="rule-sublist">Fin de donne</li>
+                <li className="rule-sublist">Fin de partie</li>
+              </ul>
+
+              <ul>
+                <li className="rule-title-list"><h4>Les cartes</h4></li>
+
+                <li className="rule-sublist">Enchères</li>
+                <li className="rule-sublist">Classement des cartes</li>
+                <li className="rule-sublist">Les Bouts/Oudlers</li>
+                <li className="rule-sublist">Le Chien & L'Écart</li>
+                <li className="rule-sublist">Les poignées</li>
+                <li className="rule-sublist">Petit au bout</li>
+              </ul>
+
+              <ul>
+                <li className="rule-title-list"><h4>Évaluation des points</h4></li>
+
+                <li className="rule-sublist">Valeurs des cartes</li>
+                <li className="rule-sublist">Distribution des points</li>
+              </ul>
+            </div>
+          </section>
+          <section className="rules-content">
+            <div className="rule-content introduction">
+              <h3>Règles du tarot Français</h3>
+              <p>bonnour fjewifjewifejfiw fiwejf weilfj lifjwe fliwej fliwejf weilfj weilfwei fl</p>
+            </div>
+          </section>
         </section>
       </section>
 
