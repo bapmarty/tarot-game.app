8264d8edf4a2e0784dfa60367023928b57241fa5
baptistemrt <contact@baptistemrt.fr>
2021-05-11 23:31:17 +0200

    :sparkles: Add new routes

--- app/src/Router/Routes.js
+++ app/src/Router/Routes.js
@@ -1,10 +1,15 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from "../Components/Home";
+import Rules from '../Components/Rules';
+import Support from '../Components/Support';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/rules" component={Rules} />
+    <Route exact path="/support" component={Support} />
+    <Redirect to="/"/>
   </Switch>
 )
 
